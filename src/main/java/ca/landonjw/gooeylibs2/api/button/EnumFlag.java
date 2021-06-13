3f2cff5580ccc95d2b2fa81bcc29be60c2c0744d
02Miki <35926600+02Miki@users.noreply.github.com>
2021-06-13 22:38:34 +0200

    Add GooeyButton.Builder#hideFlags and EnumFlag

---
+++ src/main/java/ca/landonjw/gooeylibs2/api/button/EnumFlag.java
@@ -0,0 +1,27 @@
+package ca.landonjw.gooeylibs2.api.button;
+
+public enum EnumFlag {
+    /**
+        ENCHANTS only hides tool enchants, as book ones are classified as stored enchantments.
+        EXTRAS hides multiple things, according to the minecraft wiki (https://minecraft.fandom.com/wiki/Tutorials/Command_NBT_tags)
+     */
+    PIXELMON(0),
+    ENCHANTS(1),
+    ATTRIBUTE_MODIFIERS(2),
+    UNBREAKABLE(4),
+    CAN_DESTROY(8),
+    CAN_PLACE_ON(16),
+    EXTRAS(32),
+    LEATHER_DYED(64),
+    ALL(127);
+
+    private final int value;
+
+    EnumFlag(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
--- src/main/java/ca/landonjw/gooeylibs2/api/button/GooeyButton.java
+++ src/main/java/ca/landonjw/gooeylibs2/api/button/GooeyButton.java
@@ -1,13 +1,17 @@
 package ca.landonjw.gooeylibs2.api.button;
 
+import ca.landonjw.gooeylibs2.api.template.LineType;
 import com.google.common.collect.Lists;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagList;
 import net.minecraft.nbt.NBTTagString;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 import java.util.function.Consumer;
 
 public class GooeyButton extends ButtonBase {
@@ -46,6 +50,7 @@ public class GooeyButton extends ButtonBase {
         protected String title;
         protected Collection<String> lore = Lists.newArrayList();
         protected Consumer<ButtonAction> onClick;
+        protected List<EnumFlag> hideFlags;
 
         public Builder display(@Nonnull ItemStack display) {
             this.display = display;
@@ -59,6 +64,11 @@ public class GooeyButton extends ButtonBase {
 
         public Builder lore(@Nullable Collection<String> lore) {
             this.lore = (lore != null) ? lore : Lists.newArrayList();
+            return this;
+        }
+
+        public Builder hideFlags(EnumFlag... flags) {
+            this.hideFlags = Arrays.asList(flags);
             return this;
         }
 
@@ -94,8 +104,15 @@ public class GooeyButton extends ButtonBase {
                 }
                 display.getOrCreateSubCompound("display").setTag("Lore", nbtLore);
             }
-            if (display.hasTagCompound()) {
-                display.getTagCompound().setString("tooltip", "");
+            if (!this.hideFlags.isEmpty() && display.hasTagCompound()) {
+                if (this.hideFlags.contains(EnumFlag.PIXELMON)) {
+                    display.getTagCompound().setString("tooltip", "");
+                }
+                int value = 0;
+                for (EnumFlag flag : this.hideFlags) {
+                    value += flag.getValue();
+                }
+                display.getTagCompound().setInteger("HideFlags", value);
             }
             return display;
         }
