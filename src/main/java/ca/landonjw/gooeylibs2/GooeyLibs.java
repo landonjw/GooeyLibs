20a2b3a8fce682f309939701566ff41f2534cf8e
landonjw <landon.wills@mohawkcollege.ca>
2021-07-11 14:56:38 -0400

    Fix wrong spec title in gradle, bump version to 2.2.0

--- src/main/java/ca/landonjw/gooeylibs2/GooeyLibs.java
+++ src/main/java/ca/landonjw/gooeylibs2/GooeyLibs.java
@@ -13,6 +13,6 @@ public class GooeyLibs {
 
     public static final String MOD_ID = "gooeylibs2";
     public static final String MOD_NAME = "GooeyLibs2";
-    public static final String VERSION = "2.1.1";
+    public static final String VERSION = "2.2.0";
 
 }
