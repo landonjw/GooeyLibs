75e88a83fcc360de48715abf079093a5a6c47cd0
Waterpicker <waterpickerenternity@gmail.com>
2022-04-16 03:50:06 -0500

    Forgot some debug code.

--- src/main/java/ca/landonjw/gooeylibs2/mixin/ServerPlayerMixin.java
+++ src/main/java/ca/landonjw/gooeylibs2/mixin/ServerPlayerMixin.java
@@ -13,7 +13,6 @@ public class ServerPlayerMixin extends PlayerMixin {
     @Inject(method = "doCloseContainer", at = @At(value = "INVOKE", target = "net/minecraft/world/inventory/InventoryMenu.transferState (Lnet/minecraft/world/inventory/AbstractContainerMenu;)V"), cancellable = true)
     public void transferState(CallbackInfo ci) {
         if(this.containerMenu instanceof GooeyContainer) {
-            System.out.println("This one isn't for you.");
             ci.cancel();
         }
     }
